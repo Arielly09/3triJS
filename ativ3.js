985f1a201b21b1942aaa9f6a53b255dfd0310898
Arielly09 <162440483+Arielly09@users.noreply.github.com>
2024-09-18 11:40:26 -0300

    Add files via upload

---
+++ ativ3.js
@@ -0,0 +1,18 @@
+a = 10
+b = 3
+
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b;
+
+print(f"Soma: {soma}")   13,
+print(f"Subtração: {subtracao}")    7,
+print(f"Multiplicação: {multiplicacao}") 30,
+print(f"Divisão: {divisao}")  3.333,
+
+def verificar_paridade(num):
+    return "Par" if num % 2 == 0 else "Ímpar"
+
+print(f"Paridade de a: {verificar_paridade(a)}")Par
+print(f"Paridade de b: {verificar_paridade(b)}")  Ímpar
